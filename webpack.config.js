10bf16b8a857bdbda0daeb8db3ded928887ffedc
Hans Kilian <hkkilian@gmail.com>
2017-06-07 10:21:32 +0200

    Tweaked webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,18 +18,5 @@ module.exports = {
         }
       }
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    })
-  ],
+  }
 };
